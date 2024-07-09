9ea5784d78782e7956e00ace01132365d2cb9314
Qiaochu Liu <71280341+tb-liu@users.noreply.github.com>
2024-07-09 18:53:48 -0400

    re run option 1, added ada boost

--- src/option_2/KnnReg_BaggReg_Ada.py
+++ src/option_2/KnnReg_BaggReg_Ada.py
@@ -7,7 +7,7 @@ sys.path.append(parent_dir)
 # Now you can import the module
 from read_train import *
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble import BaggingRegressor, AdaBoostRegressor
 
 if __name__ == "__main__":
     # read_file(Target.BA11, Split.S60, Normalize_Method.Log, DR_Method.ICA, Variance.V90)
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     models = {
         'K-Neighbors Regressor': KNeighborsRegressor(),
         'BaggingRegressor': BaggingRegressor(random_state=42),
+        'AdaBoostRegressor': AdaBoostRegressor(random_state=42),
     }
 
     # Define parameter grids for RandomizedSearchCV
@@ -29,21 +30,22 @@ if __name__ == "__main__":
                 'max_samples': np.linspace(0.1, 1.0, 10),
                 'max_features': np.linspace(0.1, 1.0, 10)
         },
-        
+        'AdaBoostRegressor': {
+                'n_estimators': np.arange(50, 500, 50),  # Common choices are between 50 and 500
+                'learning_rate': [0.01, 0.1, 1.0],  # Typical values are 0.01, 0.1, or 1
+                'loss': ['linear', 'square', 'exponential']  # Choices of loss function
+        },
     }
 
     # Specify which datasets to use
     combinations = filter_combinations(
-        targets=[Target.BA11, Target.BA47, Target.full],
+        targets=[Target.BA11, Target.BA47],
         splits=[Split.S60, Split.S70, Split.S80],
         n_methods=[Normalize_Method.Log, Normalize_Method.MM],
-        DR_methods=[DR_Method.KPCA],
+        DR_methods=[DR_Method.PCA, DR_Method.ICA, DR_Method.Isomap],
         variances=[Variance.V90, Variance.V95]
     )
     
-    results_df = train_test_model(models, param_grids, combinations, verbose=True, save_result=True)
-    # results_df = pd.read_csv('D:\WPI\DirectedResearch\gr-WPI-UMASS-TOD-Project\data\program_output\model_results_20240625_201447.csv')
+    results_df = train_test_model(models, param_grids, combinations, data_read_function=read_reduced_encoded_file,verbose=True, save_result=True)
     print(results_df)
-    # Convert DataFrame to list of dictionaries
-    # results_list = results_df.to_dict(orient='records')
-    write_results_to_excel(results_df, verbose=True)
+    write_results_to_excel(results_df, target_folder='performance_sheets_option2',verbose=True)

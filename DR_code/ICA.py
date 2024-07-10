91d5bd08208aead280c61a89844f6439911be4b0
Tillie Slosser <tillie@slosser.net>
2024-07-10 13:42:10 -0400

    Update ICA for Option 2

--- DR_code/ICA.py
+++ DR_code/ICA.py
@@ -4,7 +4,7 @@ import os
 import fnmatch
 import re
 import pandas as pd
-
+"""
 # For Option 1:
 BA11 = "../data/train_test_split_data/BA11"
 BA47 = "../data/train_test_split_data/BA47"
@@ -48,7 +48,7 @@ for file in os.listdir("../data/encoded"):
                         TOD_dict[region + "_" + split + "_train"] = data_dict[file][2]
                     elif ((region + "_test" not in TOD_dict.keys()) & (region in file) & (split in file) & ("test" in file)):
                         TOD_dict[region + "_" + split + "_test"] = data_dict[file][2]
-"""
+
 
 file_names = list(set(file_names))
 
@@ -100,10 +100,10 @@ for data_group in list(final_data_grouping.keys()):
 
 
     #write to csv files
-    train_file_name_95 = "../data/reduced_data/" + re.match(".+(?=_(train)|_(test)\.csv)", train_name).group() + "_ICA_95_train.csv"
-    test_file_name_95 = "../data/reduced_data/" +re.match(".+(?=_(train)|_(test)\.csv)", test_name).group() + "_ICA_95_test.csv"
-    train_file_name_90 = "../data/reduced_data/" +re.match(".+(?=_(train)|_(test)\.csv)", train_name).group() + "_ICA_90_train.csv"
-    test_file_name_90 = "../data/reduced_data/" +re.match(".+(?=_(train)|_(test)\.csv)", test_name).group() + "_ICA_90_test.csv"
+    train_file_name_95 = "../data/reduced_encoded/" + re.match(".+(?=_(train)|_(test)\.csv)", train_name).group() + "_ICA_95_train.csv"
+    test_file_name_95 = "../data/reduced_encoded/" +re.match(".+(?=_(train)|_(test)\.csv)", test_name).group() + "_ICA_95_test.csv"
+    train_file_name_90 = "../data/reduced_encoded/" +re.match(".+(?=_(train)|_(test)\.csv)", train_name).group() + "_ICA_90_train.csv"
+    test_file_name_90 = "../data/reduced_encoded/" +re.match(".+(?=_(train)|_(test)\.csv)", test_name).group() + "_ICA_90_test.csv"
 
     for region in regions:
         for split in splits:

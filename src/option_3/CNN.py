a060375d6132996f784b7b614085c5e8180c6835
Olivia Liau <olivialiau12@gmail.com>
2024-07-17 19:57:18 -0400

    update code and excel sheets, opt3 flatten

--- src/option_3/CNN.py
+++ src/option_3/CNN.py
@@ -53,7 +53,9 @@ if __name__ == "__main__":
             windows = [window] #, WindowSize.W2, WindowSize.W3]
         )
     
-        results_df = train_test_model(models, param_grids, combinations, data_read_function=read_reduced_CNN_file,verbose=True, save_result=True, use_numpy= True, data_process_function=data_processing, windows=True)
+        #set flatten to True for CNN w/o dense
+        results_df = train_test_model(models, param_grids, combinations, data_read_function=read_reduced_CNN_file,verbose=True, save_result=True, use_numpy= True, data_process_function=data_processing, windows=True, flatten = False)
         # results_df = pd.read_csv('D:\WPI\DirectedResearch\gr-WPI-UMASS-TOD-Project\data\program_output\model_results_20240708_004758.csv')
         print(results_df)
         write_results_to_excel(results_df, target_folder=f'performance_sheets_option3/{window_size_map[window]}',verbose=True)
+        # write_results_to_excel(results_df, target_folder=f'performance_sheets_option3_flatten/{window_size_map[window]}',verbose=True)

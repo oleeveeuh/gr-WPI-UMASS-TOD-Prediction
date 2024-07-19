7587c5196dcc9ec2777d32d6aed7e4705c812791
Olivia Liau <olivialiau12@gmail.com>
2024-07-19 09:37:44 -0400

    visuals

--- src/visualizations.py
+++ src/visualizations.py
@@ -12,6 +12,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from skorch import NeuralNetRegressor
 from model_definitions import LSTMRegressor, create_sequences, set_seeds
+
 random.seed(42)
 np.random.seed(42)
 torch.manual_seed(42)
@@ -21,7 +22,21 @@ torch.backends.cudnn.benchmark = False
 
 set_seeds(RANDOM_STATE)
 
-brain_areas = ('BA11','BA47')
+brain_areas = ('BA11')#,'BA47')
+
+best_combinations_BA11 = {
+    'Option 1': ('LSTM', 'PCA_90','MM', 80),
+    'Option 1 PCT': ('LSTM', 'PCA_90','log', 80),
+
+    'Option 2': ('ExtraTrees', 'Isomap_90', 'window3','MM', 80),
+    'Option 2 PCT': ('AdaBoost', 'PCA_90', 'window2','log', 80),
+
+    'Option 3': ('Random Forest', 'KPCA_90', 'window1','MM', 60),
+    'Option 3 PCT': ('AdaBoost', 'KPCA_90', 'window1','log', 80),
+
+    'Option 3 Flatten': ('Random Forest', 'KPCA_90', 'window1','MM', 60),
+    'Option 3 Flatten PCT': ('AdaBoost', 'KPCA_90', 'window1','log', 80),
+}
 
 best_models_BA11 = {
     'Option 1': 
@@ -36,68 +51,184 @@ best_models_BA11 = {
             iterator_train__shuffle=False,
             train_split=None,
             device='cpu'
-            )
-        ),
+            )),
+    'Option 1 PCT': 
+        (NeuralNetRegressor(
+                LSTMRegressor,
+                module__hidden_size=25,
+                module__num_layers=1,
+                module__output_size=1,
+                max_epochs=20,
+                batch_size = 16,
+                lr=0.1,
+                iterator_train__shuffle=False,
+                train_split=None,
+                device='cpu'
+                )),
+
     'Option 2': ExtraTreesRegressor(n_estimators = 200, 
                                      min_samples_split = 5, 
                                      min_samples_leaf =2, 
                                      max_depth =None,
-                                     random_state=RANDOM_STATE),
+                                     random_state=42
+                                     ),
+    'Option 2 PCT': AdaBoostRegressor(n_estimators=50, 
+                                  loss = 'exponential', 
+                                  learning_rate = .01,
+                                  random_state=42
+                                  ),
 
-    'Option 3': LinearRegression()
+    'Option 3': RandomForestRegressor(
+        n_estimators = 10, 
+        min_samples_split = 10, 
+        min_samples_leaf = 2,
+        max_depth = 10,
+        random_state = 42
+    ),
+    'Option 3 PCT': AdaBoostRegressor(n_estimators=200, 
+                                  loss = 'square', 
+                                  learning_rate = .01,
+                                  random_state=42
+                                  ),
+
+    'Option 3 Flatten': RandomForestRegressor(
+        n_estimators =50, 
+        min_samples_split = 2,
+        min_samples_leaf = 4, 
+        max_depth = 20,
+        random_state=42
+    ),
+    'Option 3 Flatten PCT': AdaBoostRegressor(
+        n_estimators=400, 
+        loss = 'square', 
+        learning_rate = 1.0,
+        random_state=42
+        ),
 }
+
+
+best_combinations_BA47 = {
+    'Option 1': ('LSTM', 'PCA_90', 'MM', 80),
+    'Option 1 PCT': ('LSTM', 'PCA_90','log', 80),
+
+    'Option 2': ('AdaBoost', 'PCA_95', 'window3','MM', 70),
+    'Option 2 PCT': ('AdaBoost', 'KPCA_95', 'window3','log', 80),
+
+    'Option 3': ('LSTM', 'PCA_90', 'window2','MM', 70),
+    'Option 3 PCT': ('LSTM', 'Isomap_90', 'window3','log', 80),
+
+    'Option 3 Flatten': ('LSTM', 'PCA_90', 'window2','MM', 70),
+    'Option 3 Flatten PCT': ('LSTM', 'Isomap_90', 'window3','log', 80),
+}
+
 best_models_BA47 = {
     'Option 1':
       (NeuralNetRegressor(
             LSTMRegressor,
-            module__hidden_size=25,
+            module__hidden_size=75,
             module__num_layers=1,
             module__output_size=1,
-            max_epochs=20,
+            max_epochs=30,
             batch_size = 16,
             lr=0.1,
             iterator_train__shuffle=False,
             train_split=None,
             device='cpu'
-            )
-        ),
+            )),
+    'Option 1 PCT': 
+        (NeuralNetRegressor(
+                LSTMRegressor,
+                module__hidden_size=25,
+                module__num_layers=1,
+                module__output_size=1,
+                max_epochs=20,
+                batch_size = 16,
+                lr=0.1,
+                iterator_train__shuffle=False,
+                train_split=None,
+                device='cpu'
+                )),
     'Option 2': AdaBoostRegressor(n_estimators=400, 
                                   loss = 'square', 
                                   learning_rate = 1.0,
-                                  random_state=42),
-    'Option 3': LinearRegression(),
-}
-#For Opt1: log80 for percentage metrics, MM80 for others
-#For Opt2 BA11: ADABOOST log80 PCA_90 for percentage
-#For Opt2 BA47: ADABOOST MM70 PCA_95 for non-percentage
+                                  random_state=42
+                                  ),
+    'Option 2 PCT': AdaBoostRegressor(n_estimators=400, 
+                                  loss = 'square', 
+                                  learning_rate = 1.0,
+                                  random_state=42
+                                  ),          
+    'Option 3': 
+        (NeuralNetRegressor(
+                LSTMRegressor,
+                module__hidden_size=25,
+                module__num_layers=1,
+                module__output_size=1,
+                max_epochs=20,
+                batch_size = 16,
+                lr=0.1,
+                iterator_train__shuffle=False,
+                train_split=None,
+                device='cpu'
+                )),
+    'Option 3 PCT': 
+        (NeuralNetRegressor(
+                LSTMRegressor,
+                module__hidden_size=25,
+                module__num_layers=1,
+                module__output_size=1,
+                max_epochs=20,
+                batch_size = 16,
+                lr=0.1,
+                iterator_train__shuffle=False,
+                train_split=None,
+                device='cpu'
+                )),
 
-#For Opt3 BA47: log70 for percentage metrics, log60 for others
-
-best_combinations_BA11 = {
-    'Option 1': ('LSTM', 'PCA_90','MM', 80),
-    'Option 2': ('ExtraTrees', 'Isomap_90', 'window3','MM', 80),
-    'Option 3': ('Linear', 'ICA_90', 'window2','log', 80),
-}
-
-best_combinations_BA47 = {
-    'Option 1': ('LSTM', 'PCA_90', 'log', 80),
-    'Option 2': ('AdaBoost', 'KPCA_95', 'window3','log', 80),
-    'Option 3': ('Linear', 'ICA_95', 'window3','log', 60),
+    'Option 3 Flatten': 
+        (NeuralNetRegressor(
+                LSTMRegressor,
+                module__hidden_size=25,
+                module__num_layers=1,
+                module__output_size=1,
+                max_epochs=20,
+                batch_size = 16,
+                lr=0.1,
+                iterator_train__shuffle=False,
+                train_split=None,
+                device='cpu'
+                )),
+    'Option 3 Flatten PCT':
+        (NeuralNetRegressor(
+                LSTMRegressor,
+                module__hidden_size=75,
+                module__num_layers=1,
+                module__output_size=1,
+                max_epochs=30,
+                batch_size = 16,
+                lr=0.1,
+                iterator_train__shuffle=False,
+                train_split=None,
+                device='cpu'
+                )),
 }
 
 paths = {
     'Option 1': r"data/reduced_data/",
     'Option 2': r"data/reduced_encoded/",
-    'Option 3': r"data/reduced_CNN/"
+    'Option 3': r"data/reduced_CNN/",
+    'Option 3 Flatten': r"data/reduced_CNN_flatten/"
 }
 
-
-original_data_BA11 = pd.read_csv(r"data/wrangled_data/BA11_data_6_17_2024.csv")
-original_data_BA47 = pd.read_csv(r"data/wrangled_data/BA47_data_6_17_2024.csv")
+path = ("/Users/olivialiau/Downloads/gr-WPI-UMASS-TOD-Project/data/train_test_split_data/BA11")
+original_data_60 = pd.read_csv(f"{path}/BA11_60_nonnormalized_train.csv")
+original_data_70 = pd.read_csv(f"{path}/BA11_70_nonnormalized_train.csv")
+original_data_80 = pd.read_csv(f"{path}/BA11_80_nonnormalized_train.csv")
 
 MM_cols = {
-    'BA11': original_data_BA11['TOD'],
-    'BA47': original_data_BA47['TOD']
+    60: original_data_60['TOD'],
+    70: original_data_70['TOD'],
+    80: original_data_80['TOD'],
 }
 
 # normalization_equations = {
@@ -107,20 +238,8 @@ MM_cols = {
 
 
 def get_pred():
-    if option == 'Option 1':
-        model_name, dr_method, n_method, split = (combinations_dir[option])
-        train_file = (f"{brain_area}_{split}_{n_method}_{dr_method}_train.csv")
-        test_file = (f"{brain_area}_{split}_{n_method}_{dr_method}_test.csv")
-        # print(f"Opening {train_file} and {test_file}")
-    else:
-        model_name, dr_method, window, n_method, split = (combinations_dir[option])
-        train_file = (f"{brain_area}_{split}_{n_method}_{window}_{dr_method}_train.csv")
-        test_file = (f"{brain_area}_{split}_{n_method}_{window}_{dr_method}_test.csv")
-        # print(f"Opening {train_file} and {test_file}")
-
-
-    train_path = os.path.join(paths[option], train_file)
-    test_path = os.path.join(paths[option], test_file)
+    train_path = os.path.join(paths[option[:8]], train_file)
+    test_path = os.path.join(paths[option[:8]], test_file)
         
     train_data = pd.read_csv(train_path)
     test_data = pd.read_csv(test_path)
@@ -131,7 +250,7 @@ def get_pred():
     y_test = test_data.pop('TOD')
     X_test = test_data
 
-    if option == 'Option 1':
+    if model_name == 'LSTM':
         X_train = X_train.values.astype(np.float32)
         X_test = X_test.values.astype(np.float32)
         y_train = y_train.values.astype(np.float32)
@@ -142,11 +261,11 @@ def get_pred():
     # print(f"Fitting {model}")
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    return model_name, n_method, y_test, y_pred 
+    return y_test, y_pred 
 
 
 def transform_TOD():
-    model_name, n_method, y_test, y_pred = get_pred()
+    y_test, y_pred = get_pred()
     # y_test = y_test.tolist()
     # y_pred = y_pred.tolist()
 
@@ -154,7 +273,7 @@ def transform_TOD():
     transformed_y_pred = []
     
     if n_method == 'MM':
-        col = MM_cols[brain_area]
+        col = MM_cols[split]
         # print(f"Transforming MinMax")
         for (test_value, pred_value) in zip(y_test, y_pred):
             transformed_test = (test_value * (max(col) - min(col)) + min(col))
@@ -178,33 +297,43 @@ def transform_TOD():
 
     # print(transformed_y_test)
 
-    differences = transformed_y_test - transformed_y_pred
-    std_dev = differences.std()
-    mse = mean_squared_error(transformed_y_test, transformed_y_pred)
-    mae = mean_absolute_error(transformed_y_test, transformed_y_pred)
-    mape = mean_absolute_percentage_error( transformed_y_test, transformed_y_pred)
-    rmse = mse ** 0.5
-    smape = 100 * (2 * np.abs(transformed_y_test - transformed_y_pred) / (np.abs(transformed_y_test) + np.abs(transformed_y_pred))).mean()
+    # differences = transformed_y_test - transformed_y_pred
+    # std_dev = differences.std()
+    # mse = mean_squared_error(transformed_y_test, transformed_y_pred)
+    # mae = mean_absolute_error(transformed_y_test, transformed_y_pred)
+    # mape = mean_absolute_percentage_error( transformed_y_test, transformed_y_pred)
+    # rmse = mse ** 0.5
+    # smape = 100 * (2 * np.abs(transformed_y_test - transformed_y_pred) / (np.abs(transformed_y_test) + np.abs(transformed_y_pred))).mean()
     
-    print(f"{brain_area} {option}: {model_name}")
-    print(f"STD: {std_dev}\nMSE: {mse}\nMAE: {mae}\nMAPE: {mape}\nRMSE: {rmse}\nSMAPE: {smape}")
+    # print(f"{brain_area} {option}: {model_name}")
+    # print(f"STD: {std_dev}\nMSE: {mse}\nMAE: {mae}\nMAPE: {mape}\nRMSE: {rmse}\nSMAPE: {smape}")
 
-    add_graph(model_name, transformed_y_test.tolist(), transformed_y_pred.tolist())
+    # add_graph(transformed_y_test.tolist(), transformed_y_pred.tolist())
     return transformed_y_test, transformed_y_pred
 
 
-def add_graph(model_name, y_test, y_pred):
+def add_graph():
+    y_test, y_pred = transform_TOD()
     # i = np.arange(0,len(y_pred))
-    ax[x, y].plot(y_test, y_pred, 'r-', label = "Predicted")
-    ax[x, y].plot(y_test, y_test, 'g-', label = "Actual")
-    ax[x, y].set_title(f'{brain_area} {option}: {model_name}')
+    ax[x, y].scatter(y_test, y_pred, s=10, c='red', marker='o', label='Predicted')
+    ax[x, y].plot(y_test, y_test, c='green', label='Actual')
+    ax[x, y].set_title(f'{brain_area} {option}: {model_name} w/ {dr_method} & {n_method}{split}')
+    ax[x, y].set_xlabel('Actual TOD')
+    ax[x, y].set_ylabel('TOD')
+    ax[x, y].legend()
 
-    plt.legend() 
+    differences = y_test - y_pred
+    std_dev = differences.std()
+    mae = mean_absolute_error(y_test, y_pred)
+    print(f"MAE: {mae}")
+    ax[x, y].text(0.97, 0.10, f"STD: {std_dev:.3f}", ha='right',va = 'top', fontsize=13, transform=ax[x, y].transAxes, bbox=dict(facecolor='pink', alpha=.6))
 
 
-y = 0
-fig, ax = plt.subplots(3, 2, figsize=(10, 8), layout="constrained")
+
+
 for brain_area in brain_areas:
+    fig, ax = plt.subplots(4, 2, figsize=(12, 16), layout="constrained")
+    y = 0
     x = 0
     if brain_area == 'BA11':
         model_dir = best_models_BA11
@@ -215,9 +344,27 @@ for brain_area in brain_areas:
 
     
     for option, model in model_dir.items():
-        transform_TOD()
-        x+=1
 
-    y=1
+        if option == 'Option 1' or option == "Option 1 PCT":
+            model_name, dr_method, n_method, split = (combinations_dir[option])
+            train_file = (f"{brain_area}_{split}_{n_method}_{dr_method}_train.csv")
+            test_file = (f"{brain_area}_{split}_{n_method}_{dr_method}_test.csv")
+        # print(f"Opening {train_file} and {test_file}")
+        else:
+            model_name, dr_method, window, n_method, split = (combinations_dir[option])
+            train_file = (f"{brain_area}_{split}_{n_method}_{window}_{dr_method}_train.csv")
+            test_file = (f"{brain_area}_{split}_{n_method}_{window}_{dr_method}_test.csv")
+            # print(f"Opening {train_file} and {test_file}")
 
-plt.show()
+        add_graph()
+        # transform_TOD()
+        if y == 0: y=1
+        elif y == 1: 
+            y=0
+            x+=1
+    # plt.subplots_adjust(right=0.85)
+    # plt.tight_layout()
+    plt.savefig(f"{brain_area}_plot.png")
+
+
+
